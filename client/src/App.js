17591bd009d6be6d3d0827fe08f0ec0a65a41578
Oscar Bryant <oscarcbryant@gmail.com>
2022-05-10 19:18:13 +1000

    adding auth jwt

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,25 @@ import Body from './components/Body';
 import Footer from './components/footer';
 import Player from './components/showPlayers/showPlayers.js';
 import Create from './components/createPlayers/createPlayers.js'
-
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 
 function App() {
   return (
     <Router>
       <Header />
-      <Navbar />
-      <Routes>
-      </Routes>
+       <Navbar />
+          <Routes>
+            <Route 
+              path="/login" 
+              element={<Login />}
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />}
+            />
+          </Routes>
         <Body />
         <Player />
         <Create />
